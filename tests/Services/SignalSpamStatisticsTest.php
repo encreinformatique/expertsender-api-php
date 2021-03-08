5fe1d80e89f9e80cdb96d018fa3b9dfc314ea437
Julien Devergnies <j.devergnies@outlook.com>
2021-03-08 19:32:32 +0100

    fixed : test of SignalSpamStatistics dealing with message
    SignalSpamStatisticsTest had to manager only SignalSpamStatistics. It was messing with Message as well.

--- tests/Services/SignalSpamStatisticsTest.php
+++ tests/Services/SignalSpamStatisticsTest.php
@@ -2,144 +2,101 @@
 
 namespace ExpertSenderFr\ExpertSenderApi\Tests\Services;
 
+use ExpertSenderFr\ExpertSenderApi\ApiResponse;
+use ExpertSenderFr\ExpertSenderApi\Model\SignalSpamReport;
+use ExpertSenderFr\ExpertSenderApi\Test\FakeApiRequest;
+use ExpertSenderFr\ExpertSenderApi\Test\TestClient;
 use PHPUnit\Framework\TestCase;
-use ExpertSenderFr\ExpertSenderApi\ExpertSenderClient;
-use ExpertSenderFr\ExpertSenderApi\Model\Message;
-use ExpertSenderFr\ExpertSenderApi\Model\NewsletterCreationPayload;
 
 class SignalSpamStatisticsTest extends TestCase
 {
     /**
      * @test
      */
-    public function canQuery()
+    public function canQuery(): void
     {
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
+        $responseBody = <<<EOF
+<ApiResponse xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+   <Data>
+      <SignalSpamStatistics>
+         <SignalSpamStatistic>
+            <IsSummaryRow>true</IsSummaryRow>
+            <Date xsi:nil="true"/>
+            <Delivered>0</Delivered>
+            <Complaints>1332</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>1110</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-18</Date>
+            <Delivered>0</Delivered>
+            <Complaints>0</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>0</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-17</Date>
+            <Delivered>0</Delivered>
+            <Complaints>0</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>0</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-16</Date>
+            <Delivered>0</Delivered>
+            <Complaints>0</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>0</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-15</Date>
+            <Delivered>0</Delivered>
+            <Complaints>0</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>0</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-14</Date>
+            <Delivered>0</Delivered>
+            <Complaints>888</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>333</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-13</Date>
+            <Delivered>0</Delivered>
+            <Complaints>333</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>222</Spamtraps>
+         </SignalSpamStatistic>
+         <SignalSpamStatistic>
+            <Date>2015-10-12</Date>
+            <Delivered>0</Delivered>
+            <Complaints>111</Complaints>
+            <ComplaintRate>0.00</ComplaintRate>
+            <Spamtraps>555</Spamtraps>
+         </SignalSpamStatistic>
+      </SignalSpamStatistics>
+   </Data>
+</ApiResponse>
+EOF;
+        $response = new ApiResponse($responseBody, 200, []);
+        $request = new FakeApiRequest();
+        $request->setResponse($response);
+
+        $client = new TestClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
+        $client->setNextRequest($request);
         $service = $client->signalSpamStatistics();
 
-        $response = $service->get(null, null, ['grouping' => 'Provider']);
+        $res = $service->get(null, null, ['grouping' => 'Provider']);
 
-        $this->assertTrue(true);
-    }
-
-    public function testDeleteOnInstantiation()
-    {
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
-        $service = $client->messages();
-
-        $this->assertTrue($service->delete(341));
-    }
-
-    public function testDeleteWithApiKeyAsOption()
-    {
-        $client = new ExpertSenderClient(null, 'https://api2.esv2.com');
-        $service = $client->messages();
-
-        $this->assertTrue($service->delete(341, ['api_key' => '<YOUR_API_KEY_HERE>']));
-    }
-
-    public function testDeleteWithUrlAsOption()
-    {
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>');
-        $service = $client->messages();
-
-        $this->assertTrue($service->delete(341, ['domain' => 'https://api2.esv2.com']));
-    }
-
-    public function testDeleteWithApiKeyAndUrlAsOption()
-    {
-        $client = new ExpertSenderClient();
-        $service = $client->messages();
-
-        $this->assertTrue($service->delete(341, ['api_key' => '<YOUR_API_KEY_HERE>', 'domain' => 'https://api2.esv2.com']));
-    }
-
-    public function testDeleteThrowsExceptionIfNoApiKeyIsSet()
-    {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('API Key not set.');
-
-        $client = new ExpertSenderClient();
-        $service = $client->messages();
-
-        $service->delete(341, ['domain' => 'https://api2.esv2.com']);
-    }
-
-    public function testDeleteThrowsExceptionIfNoDomainIsSet()
-    {
-        $this->expectException(\Symfony\Component\OptionsResolver\Exception\InvalidOptionsException::class);
-
-        $client = new ExpertSenderClient();
-        $service = $client->messages();
-
-        $service->delete(341, ['api_key' => '<YOUR_API_KEY_HERE>']);
-    }
-
-    public function testDeleteThrowsExceptionIfDomainIsEmpty()
-    {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('The domain cannot be an empty string.');
-
-        $client = new ExpertSenderClient();
-        $service = $client->messages();
-
-        $service->delete(341, ['api_key' => '<YOUR_API_KEY_HERE>', 'domain' => '']);
-    }
-
-    public function testCreateNewsletterThrowsExceptionWhenNoRecipientIsSet()
-    {
-        $this->expectException(\ExpertSenderFr\ExpertSenderApi\NoRecipientsException::class);
-
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
-        $service = $client->messages();
-
-        $message = new NewsletterCreationPayload('Test', 'Tester', 'test@example.org');
-        $service->createNewsletter($message);
-    }
-
-    public function testCreateNewsletter()
-    {
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
-        $service = $client->messages();
-
-        $message = new NewsletterCreationPayload('Test', 'Tester', 'test@example.org');
-        $message->addSubscriberList(32);
-        $message->setMessageContent('', 'test');
-
-        $message->setDeliveryDate('2017-09-12', 'UTC');
-
-        $messageId = $service->createNewsletter($message);
-
-        $this->assertEquals('integer', gettype($messageId));
-        $service->delete($messageId);
-    }
-
-    public function testPauseNewsletter()
-    {
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
-        $service = $client->messages();
-
-        $message = new NewsletterCreationPayload('Test', 'Tester', 'test@example.org');
-        $message->addSubscriberList(32);
-        $message->setMessageContent('', 'test');
-
-        $message->setDeliveryDate('2017-09-12', 'UTC');
-
-        $messageId = $service->createNewsletter($message);
-        $resultCode = $service->pause($messageId);
-
-        $this->assertSame(0, $resultCode);
-        $service->delete($messageId);
-    }
-
-    public function testGetNewsletter()
-    {
-        $client = new ExpertSenderClient('<YOUR_API_KEY_HERE>', 'https://api2.esv2.com');
-        $service = $client->messages();
-
-        $message = $service->get(351);
-
-        $this->assertInstanceOf(Message::class, $message);
-        $this->assertEquals(351, $message->getId());
+        self::assertIsArray($res);
+        self::assertCount(8, $res);
+        self::assertInstanceOf(SignalSpamReport::class, $res[0]);
+        self::assertTrue($res[0]->isSummaryRow);
+        self::assertEquals(0, $res[0]->delivered);
+        self::assertEquals(1332, $res[0]->complaints);
+        self::assertEquals(0.00, $res[0]->complaintRate);
+        self::assertEquals(1110, $res[0]->spamTraps);
     }
 }
